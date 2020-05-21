05b246d572c7ab0c81faec324b5de776c33faff1
Ahmad Gneady <support@bigprof.com>
2020-05-21 20:21:25 +0200

    Move Friday pocket money functions to a separate file to be called via a cronjob.

--- app/hooks/__global.php
+++ app/hooks/__global.php
@@ -101,3 +101,48 @@
 	function sendmail_handler(&$pm) {
 
 	}
+
+	function auto_deposit_pocket_money_weekly() {
+		$date = pocket_money_date();
+		$description = 'Pocket money';
+
+		// get all kids and their current pocket money value
+		$kids = get_all_kids();
+		foreach ($kids as $kid) {
+			// if pocket money already added, skip
+			if(sqlValue(
+				"SELECT COUNT(1) FROM `transactions` 
+				 WHERE
+				 	`kid` = '{$kid['id']}' AND
+				 	`date` = '{$date}' AND
+				 	`description` LIKE '{$description}'"
+			)) continue;
+
+			// insert pocket money for kid, then set owner to admin
+			if(insert('transactions', [
+				'kid' => $kid['id'],
+				'date' => $date,
+				'amount' => $kid['pocket_money'],
+				'description' => $description
+			]))
+				set_record_owner('transactions', db_insert_id(), 'admin');
+		}
+	}
+
+	function get_all_kids() {
+		$kids = [];
+		$res = sql("SELECT * FROM `kids`", $eo);
+		while($row = db_fetch_assoc($res)){
+			$kids[] = $row;
+		}
+
+		return $kids;
+	}
+
+	function pocket_money_date() {
+		// if today is Friday, return today
+		if(date('w') == 5) return date('Y-m-d');
+
+		// return date of last Friday
+		return date('Y-m-d', strtotime('last friday'));
+	}
---
+++ app/hooks/cron-pocket-money-friday.php
@@ -0,0 +1,7 @@
+<?php
+	$hooks_dir = dirname(__FILE__);
+	include("{$hooks_dir}/../defaultLang.php");
+	include("{$hooks_dir}/../language.php");
+	include("{$hooks_dir}/../lib.php");
+	
+	auto_deposit_pocket_money_weekly();
