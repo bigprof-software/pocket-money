f8910ed0ce9e3d859bfdf560f35ec5637e13c743
Ahmad Gneady <support@bigprof.com>
2020-06-14 14:31:17 +0200

    Fix bug where button didn't work in mobile view

--- app/hooks/kids-tv.js
+++ app/hooks/kids-tv.js
@@ -1,12 +1,12 @@
 $j(function() {
-	$j('<button id="add-pocket-money" type="button" class="btn btn-default"><i class="glyphicon glyphicon-usd"></i> Weekly pocket money</button>').appendTo('.all_records');
+	$j('<button type="button" class="add-pocket-money btn btn-default"><i class="glyphicon glyphicon-usd"></i> Weekly pocket money</button>').appendTo('.all_records');
 	
-	$j('#add-pocket-money').on('click', function() {
+	$j('.add-pocket-money').on('click', function() {
 		$j(this).prop('disabled', true);
 		$j.ajax({
 			url: 'hooks/cron-pocket-money-friday.php',
 			complete: function() {
-				$j('#add-pocket-money').prop('disabled', false);
+				$j('.add-pocket-money').prop('disabled', false);
 			}
 		})
 	})
