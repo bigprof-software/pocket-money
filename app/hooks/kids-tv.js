661f5452236d6821e9924c7df0a0f4b803599f7d
Ahmad Gneady <support@bigprof.com>
2020-06-14 14:22:24 +0200

    Add button link to update weekly pocket money

---
+++ app/hooks/kids-tv.js
@@ -0,0 +1,13 @@
+$j(function() {
+	$j('<button id="add-pocket-money" class="btn btn-default"><i class="glyphicon glyphicon-usd"></i> Weekly pocket money</button>').appendTo('.all_records');
+	
+	$j('#add-pocket-money').on('click', function() {
+		$j(this).prop('disabled', true);
+		$j.ajax({
+			url: 'hooks/cron-pocket-money-friday.php',
+			complete: function() {
+				$j('#add-pocket-money').prop('disabled', false);
+			}
+		})
+	})
+})
