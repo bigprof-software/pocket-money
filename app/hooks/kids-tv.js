655737296af0463715b6f477109bd9356a7c7f65
Ahmad Gneady <support@bigprof.com>
2020-06-14 14:23:40 +0200

    Prevent page reload on clicking pocket money button

--- app/hooks/kids-tv.js
+++ app/hooks/kids-tv.js
@@ -1,5 +1,5 @@
 $j(function() {
-	$j('<button id="add-pocket-money" class="btn btn-default"><i class="glyphicon glyphicon-usd"></i> Weekly pocket money</button>').appendTo('.all_records');
+	$j('<button id="add-pocket-money" type="button" class="btn btn-default"><i class="glyphicon glyphicon-usd"></i> Weekly pocket money</button>').appendTo('.all_records');
 	
 	$j('#add-pocket-money').on('click', function() {
 		$j(this).prop('disabled', true);
