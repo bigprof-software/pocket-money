784e0e3d5f1547bd35f8116c9e0fdb8dfade1cf0
Ahmad Gneady <support@bigprof.com>
2021-12-27 02:43:39 +0200

    update custom file to new appgini includes

--- app/hooks/cron-pocket-money-friday.php
+++ app/hooks/cron-pocket-money-friday.php
@@ -1,8 +1,5 @@
 <?php
-	$hooks_dir = dirname(__FILE__);
-	include("{$hooks_dir}/../defaultLang.php");
-	include("{$hooks_dir}/../language.php");
-	include("{$hooks_dir}/../lib.php");
+	include(__DIR__ . "/../lib.php");
 	
 	$debug = [];
 	auto_deposit_pocket_money_weekly($debug);
